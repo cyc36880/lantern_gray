07d7e42717c35bfce440e32de96ccc19dff0fb1e
ZIShen <2672632650@qq.com>
2025-08-25 17:38:38 +0800

    [perf] 对超声波测量值滤波

--- src/myCode/myCode/myMain.c
+++ src/myCode/myCode/myMain.c
@@ -4,6 +4,7 @@
 #include "register.h"
 #include "uart.h"
 
+static const uint8_t filter_depth = 3;
 static const uint8_t ultr_start = 0xA0;
 
 void setup(void)
@@ -23,12 +24,15 @@ void setup(void)
 
 void loop(void)
 {
+    static uint8_t uart_rev[filter_depth][3] = {0};
+    static uint8_t sampling_count = 0;
+    
     if (1 == sys_register.changle_flag)
     {
         sys_register.changle_flag = 0;
-        uint8_t r = sys_register.reg[2 + 0] * sys_register.reg[1] / 255;
-        uint8_t g = sys_register.reg[2 + 1] * sys_register.reg[1] / 255;
-        uint8_t b = sys_register.reg[2 + 2] * sys_register.reg[1] / 255;
+        uint8_t r = (uint32_t)sys_register.reg[2 + 0] * sys_register.reg[1] / 255;
+        uint8_t g = (uint32_t)sys_register.reg[2 + 1] * sys_register.reg[1] / 255;
+        uint8_t b = (uint32_t)sys_register.reg[2 + 2] * sys_register.reg[1] / 255;
         for (uint16_t i=0; i<WS2812_NUM; i++)
         {
             ws2812_set_color(i, r, g, b);
@@ -36,12 +40,40 @@ void loop(void)
         ws2812_sync_display();
     }
     uart_send_bytes(&huart2, (uint8_t *)&ultr_start, 1);
-    uint8_t uart_rev[3];
-    if (uart_receive_bytes(&huart2, uart_rev, 3, 200) == 0)
+    
+    
+    if (uart_receive_bytes(&huart2, uart_rev[sampling_count], 3, 200) == 0)
     {
-        ultr_register.reg[0] = uart_rev[0];
-        ultr_register.reg[1] = uart_rev[1];
-        ultr_register.reg[2] = uart_rev[2];
+        if (++sampling_count >= filter_depth) sampling_count=0;
+        
+    }
+    if (0 == sampling_count)
+    {
+        // 对uart_rev[x]冒泡排序
+        for (uint8_t i=0; i<filter_depth; i++)
+        {
+            uint32_t temp_val = uart_rev[i][0] << 16 | uart_rev[i][1] << 8 | uart_rev[i][2];
+            for (uint8_t j=i+1; j<filter_depth; j++)
+            {
+                uint32_t next_val = uart_rev[j][0] << 16 | uart_rev[j][1] << 8 | uart_rev[j][2];
+                if (next_val < temp_val)
+                {
+                    // 交换
+                    uint8_t tmp[3] = {uart_rev[i][0], uart_rev[i][1], uart_rev[i][2]};
+                    uart_rev[i][0] = uart_rev[j][0];
+                    uart_rev[i][1] = uart_rev[j][1];
+                    uart_rev[i][2] = uart_rev[j][2];
+                    uart_rev[j][0] = tmp[0];
+                    uart_rev[j][1] = tmp[1];
+                    uart_rev[j][2] = tmp[2];
+                    temp_val = next_val;
+                }
+            }
+        }
+
+        ultr_register.reg[0] = uart_rev[filter_depth/2][0];
+        ultr_register.reg[1] = uart_rev[filter_depth/2][1];
+        ultr_register.reg[2] = uart_rev[filter_depth/2][2];
     }
 }
 
